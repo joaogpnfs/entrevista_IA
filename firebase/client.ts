f6524ea093cf75deb5021fb4901d9194ddf8dec8
joaogpnfs <jgpnfeitosa57@gmail.com>
2025-05-02 22:36:31 -0300

    fix: firebase config client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,16 +3,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBCM4bCMxligA4XwSFYNSVQcorWQy7pW2Y",
-  authDomain: "preptalk-cb2f2.firebaseapp.com",
-  projectId: "preptalk-cb2f2",
-  storageBucket: "preptalk-cb2f2.firebasestorage.app",
-  messagingSenderId: "863852552434",
-  appId: "1:863852552434:web:94493354b17fc074b5f06b",
-  measurementId: "G-SQ1RVB471L",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
